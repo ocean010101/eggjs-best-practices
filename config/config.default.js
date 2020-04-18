04e5b1a45f98f20ae0e6f03af57d3194600d244f
ocean010101 <yangyang01.xu@outlook.com>
2020-04-18 20:43:37 +0800

    Add exception handling
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- config/config.default.js
+++ config/config.default.js
@@ -16,7 +16,7 @@ module.exports = appInfo => {
   config.keys = appInfo.name + '_1586555566673_8463';
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = ['errorHandler'];
   config.swaggerdoc = {
     dirScanner: './app/controller',
     apiInfo: {
